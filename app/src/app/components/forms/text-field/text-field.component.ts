4c107d82deb235a5ae0f51f644e0913b9bd47d4b
Dan Kurin <dan@leapfinder.com>
2024-03-23 19:59:54 -0400

    Add generic text input, start building Signup form

---
+++ app/src/app/components/forms/text-field/text-field.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormValidation } from '../../../interfaces/form-validation';
+
+@Component({
+  selector: 'app-text-field',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './text-field.component.html',
+  styleUrl: './text-field.component.scss'
+})
+export class TextFieldComponent {
+  @Input('name') name!: string;
+  @Input('form') form!: FormGroup;
+  @Input('validation') validation!: FormValidation[];
+  @Input('type') type: string = 'text';
+  @Input('label') label?: string;
+  @Input('placeholder') placeholder?: string;
+  @Input('classes') classes?: string;
+
+}
--- app/src/app/pages/sign-up/sign-up.page.ts
+++ app/src/app/pages/sign-up/sign-up.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { TextFieldComponent } from "../../components/forms/text-field/text-field.component";
 
 @Component({
-  selector: 'app-sign-up',
-  standalone: true,
-  imports: [],
-  templateUrl: './sign-up.page.html',
-  styleUrl: './sign-up.page.scss'
+    selector: 'app-sign-up',
+    standalone: true,
+    templateUrl: './sign-up.page.html',
+    styleUrl: './sign-up.page.scss',
+    imports: [
+        ReactiveFormsModule,
+        TextFieldComponent
+    ]
 })
 export class SignUpPage {
+  signupForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required])
+  }, { validators: this.checkPasswords });
 
+  constructor(
+    private api: ApiService
+  ) {}
+
+  signup() {
+
+  }
+
+  checkPasswords (group: AbstractControl):  ValidationErrors | null { 
+    let pass = group.get('password')?.value;
+    let confirmPass = group.get('confirmPassword')?.value
+    return pass === confirmPass ? null : { notSame: true }
+  }
 }
