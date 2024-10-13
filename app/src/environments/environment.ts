4bf82f20d6bed6f0f302ff72cb0f43b2ae02502a
Dan Kurin <dan@leapfinder.com>
2024-10-13 17:11:37 -0400

    Prod deploy

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -3,7 +3,7 @@ import { Environment } from "@app/../../interfaces/environment";
 export const environment:Environment = {
     production: true,
     appUrl: "https://app.2dingit.com/",
-    supabaseUrl: "https://swwcfdyovdopswxkyejf.supabase.co",
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3d2NmZHlvdmRvcHN3eGt5ZWpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAwMjg4NDAsImV4cCI6MjAyNTYwNDg0MH0.vJ_m06WyipmhSLSj_qN3vKKJkysDxh3b6ByngFM9P48",
+    supabaseUrl: "https://dcsndfnfbcqcavrfpprw.supabase.co",
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjc25kZm5mYmNxY2F2cmZwcHJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg4NTMxMzcsImV4cCI6MjA0NDQyOTEzN30.piInrFaUpXHN9koMFvitZwbqHzGowKqkYIlvjxmKo6U",
     stripeKey: "pk_live_51PoWC1J2uBT3FJoZOYZw9BvVWWjeeU3fja3czHWZpBAohMZZc0S1eoJHUEmZ710Cfv1N0HCZtBvdBz8uF6Mjgt0H00mqjcgIgU"
 };
